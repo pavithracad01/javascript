d353cf2a7f85acfc4ee7d3202a9665a81500ed8e
Pavithra K <pavithracad01@gmail.com>
2024-01-05 15:26:33 +0530

    jsfileadded

---
+++ samplejs.js
@@ -0,0 +1,48 @@
+console.log("welcome")
+let a=8;
+var b=10;
+const c=25;
+console.log(a);
+console.log(b);
+console.log(c);
+//object 
+e={fname:"pavithra",gender:"female"};
+console.log(e);
+console.log(typeof e);
+
+//typeof value
+a=5;
+console.log(a);
+console.log(typeof a);
+//string
+b="pavithra";
+console.log(b);
+console.log(typeof b);
+console.log(b+ " : "+typeof b);
+d=[5,50,69,45];
+console.log(d);
+console.log(typeof d);
+a=true;
+console.log(typeof a);
+console.log(a+ " : "+typeof a);
+e={fname:"pavithra",gender:"female"};
+console.log(typeof e);
+//type conversion
+a=20;
+console.log(typeof a);
+a=a.toString();
+console.log(typeof a);
+a=parseInt(a);
+console.log(typeof a);
+b="90"
+b=parseFloat(b);
+console.log(b);
+
+a="60"
+d=90;
+console.log(typeof a);
+a=a+d;
+console.log(a);
+
+
+
